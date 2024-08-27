8e00208600a7b97dc5267b1bb16b16e333fcca29
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-08-28 01:55:53 +0500

    Normal Config

---
+++ lua/vim-options.lua
@@ -0,0 +1,14 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+vim.opt.breakindent = true
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+
